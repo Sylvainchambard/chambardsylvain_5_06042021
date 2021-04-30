68fd558225720be11c8a72c9a2163ebfe697b62c
Sylvain Chambard <sylvain.70.400@gmail.com>
2021-04-30 16:17:06 +0200

    select option

--- produit.js
+++ produit.js
@@ -12,26 +12,65 @@ const id = urlSearchParams.get("id")
     .then(info => {
         let name = document.getElementById("name")
         name.innerText = info.name
+        
         let price = document.getElementById("price")
         price.innerText ="Prix : "+ info.price/100 +" €"
+        
         let description = document.getElementById("description")
         description.innerText = info.description
+        
         let image = document.getElementById("picture")
         image.src = info.imageUrl 
+        
         let colors = document.getElementById("info_colors")
         colors.innerText = `Choisissez parmis ses ${info.colors.length} couleurs différentes `
          
         let allColors = [];
         for (let i = 0; i < info.colors.length; i++) {
           allColors += `<option value=${info.colors[i]}>${info.colors[i]}</option> `
-            
+          
           let inner = document.getElementById("option");
           inner.innerHTML = allColors;
       
+        //Selection de l'id du formulaire
+        const idForm = document.querySelector("#option")
+         
+        // Selection du bouton
+        let btnEnvoyerPanier = document.getElementById("btn_envoyer")
+       
+        
+        //Ecouter le bouton et envoyer au panier
+        btnEnvoyerPanier.addEventListener("click", (event)=>{
+          event.preventDefault()
+          console.log(btnEnvoyerPanier)
+        
+
+        //mettre le choix dans une variable
+        const choixForm = idForm.value
+       
+        //Récupération des valeurs du formulaire
+        let optionProduit = {
+          nom : info.name,
+          photo : info.imageUrl,
+          option : info.colors,
+          idProduit : info._id,
+          quantite : 1,
+          prix : info.price/100
         }
-
+        
         console.log(allColors)
+        console.log(optionProduit)
 
-    })
 
-  
+       
+ 
+        })
+ 
+    
+        
+           }
+ 
+   })
+
+
+ 
